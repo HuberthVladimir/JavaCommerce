2bfce7cb5fa6cdc9430a5114884ff5c53f804824
HuberthVladimir <vladimirhuberth@hotmail.com>
2024-11-28 19:09:44 -0300

    first commit

---
+++ src/test/java/com/hubethvladimir/JavaCommerce/JavaCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hubethvladimir.JavaCommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
